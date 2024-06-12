f1b8827e8837e543c6938d76cee8e9983ac28920
ultraman <1394466835@qq.com>
2024-06-12 13:50:09 +0800

    update

---
+++ computer-system/bits_bytes/beep_beep_boop/beep.py
@@ -0,0 +1,14 @@
+import sys
+import tty
+
+tty.setcbreak(0)
+
+while True:
+    char = sys.stdin.read(1)
+    try:
+        n_times = int(char)
+    except ValueError:
+        n_times = 0
+    for _ in range(n_times):
+        sys.stdout.buffer.write(b"\x07")
+    sys.stdout.buffer.flush()
