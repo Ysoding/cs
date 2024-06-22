0d74bb8968399ab92761d49b86798fbbbedb61fe
ultraman <1394466835@qq.com>
2024-06-22 18:21:38 +0800

    update

--- computer-system/intro_to_c/varint_extension/README.md
+++ computer-system/intro_to_c/varint_extension/README.md
@@ -2,9 +2,22 @@
 ## Run 
 
 ```sh
+python -m venv .venv
+source .venv/bin/activate
 pip install build setuptools
 # build extension
 python -m build
 # install extension
-pip install ./dist/cvarint-1.0.0-cp312-cp312-linux_aarch64.whl
+pip install ./dist/cvarint-1.0.0-cp310-cp310-linux_x86_64.whl
+
+```
+
+
+```sh
+python test.py 
+Running speed test...
+Executed 1,000,000 random tests
+
+Python:   2766ns per case (2.767s total)
+C:         419ns per case (0.420s total)
 ```
--- computer-system/intro_to_c/varint_extension/cvarintmodule.c
+++ computer-system/intro_to_c/varint_extension/cvarintmodule.c
@@ -1,3 +1,57 @@
-#include <stdio.h>
+#define PY_SSIZE_T_CLEAN
+#include <Python.h>
 
-int main() {}
+static PyObject *cvarint_encode(PyObject *self, PyObject *args) {
+  unsigned long long n = 0;
+
+  if (!PyArg_ParseTuple(args, "K", &n)) {
+    return NULL;
+  }
+
+  char part;
+  char out[10];
+  int i = 0;
+
+  while (n > 0) {
+    part = n & 0x7F;
+    n >>= 7;
+    if (n > 0) {
+      part |= 0x80;
+    }
+
+    out[i++] = part;
+  }
+
+  return PyBytes_FromStringAndSize(out, i);
+}
+
+static PyObject *cvarint_decode(PyObject *self, PyObject *args) {
+  Py_buffer varints;
+
+  if (!PyArg_ParseTuple(args, "y*", &varints)) {
+    return NULL;
+  }
+
+  unsigned long long n = 0;
+  char *data = varints.buf;
+  Py_ssize_t length = varints.len;
+
+  for (int i = length - 1; i >= 0; i--) {
+    n <<= 7;
+    n |= (data[i] & 0x7F);
+  }
+
+  return PyLong_FromUnsignedLongLong(n);
+}
+
+static PyMethodDef CVarintMethods[] = {
+    {"encode", cvarint_encode, METH_VARARGS, "Encode an integer as varint."},
+    {"decode", cvarint_decode, METH_VARARGS,
+     "Decode varint bytes to an integer."},
+    {NULL, NULL, 0, NULL}};
+
+static struct PyModuleDef cvarintmodule = {
+    PyModuleDef_HEAD_INIT, "cvarint",
+    "A C implementation of protobuf varint encoding", -1, CVarintMethods};
+
+PyMODINIT_FUNC PyInit_cvarint(void) { return PyModule_Create(&cvarintmodule); }
